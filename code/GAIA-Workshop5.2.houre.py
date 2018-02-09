b3f9964f5ab9e099ecb707798b5b139784714ca0
LPFraile <lidiapf0@gmail.com>
2018-02-09 17:01:23 +0200

    temeprature worksho

---
+++ code/GAIA-Workshop5.2.houre.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+from threading import Thread
+import time
+
+import gaia_text
+import properties
+import sparkworks
+import grovepi
+from grove_rgb_lcd import *
+import math
+
+import arduinoGauge
+import datetime
+exitapp = False
+timestamp=0
+main_site = None
+sensorValues=[0,0,0]
+
+#select pins for the leds
+pin1 = [2, 4]
+pin2 = [3, 5]
+
+#select colors for the rooms
+R = [255, 255, 0]
+G = [0, 128, 255]
+B = [255, 0, 0]
+
+text = ""
+new_text = ""
+
+Button1=8
+Button2=7
+grovepi.pinMode(Button1, "INPUT")
+grovepi.pinMode(Button2, "INPUT")
+for i in [0, 1]:
+    grovepi.pinMode(pin1[i], "OUTPUT")
+    grovepi.pinMode(pin2[i], "OUTPUT")
+
+
+
+#initiliaze the LCD screen color and value
+text=gaia_text.loading_data
+setText(text)
+setRGB(60, 60, 60)
+
+
+def updateData(site,param):
+    global timestamp, maximum
+    resource=sparkworks.siteResource(site,param)	
+    summary = sparkworks.summary(resource)
+    val = summary["minutes60"]
+    #print val
+    timestamp=summary["latestTime"]
+    return (val)
+
+def getSensorData(SensorName):
+    global sensorValues
+    if not exitapp:
+	for i in[0,1,2]:
+		val=updateData(rooms[i],SensorName)
+		sensorValues[i]=val
+
+#Show the  temperature
+def showTemperature(temperature_value, a, b):
+    if 18 < temperature_value < 25:
+        # BLue LED
+        grovepi.digitalWrite(a, 1)
+        grovepi.digitalWrite(b, 0)
+    else:
+        # red led
+        grovepi.digitalWrite(a, 0)
+        grovepi.digitalWrite(b, 1)
+
+
+
+#Print rooms
+print "όνομα χρήστη:\n\t%s\n" % properties.username
+print "Επιλεγμένη αίθουσα:"
+for room in properties.the_rooms:
+    print '\t%s' % room.decode('utf-8')
+print '\n'
+
+
+#total Power
+sparkworks.connect(properties.username, properties.password)
+rooms = sparkworks.select_rooms(properties.the_rooms)
+new_text="Click button to start!"
+
+
+def main():
+    	global text, new_text, timestamp, sensorValues	
+   	time.sleep(1)
+        t=0
+	new_t=0
+	rm=0
+	val=0	
+    	while not exitapp:
+		#detect Button that choose houre
+		try:
+			if (grovepi.digitalRead(Button1)):
+				setText("New Houre")
+				t=t+1
+				if t==24:
+					setText("Take new data")
+					t=0
+				time.sleep(.4)
+    		except IOError:
+      			print "Button Error"
+		#Detect the button that choose room
+		try:
+                        if (grovepi.digitalRead(Button2)):
+                                rm=rm+1
+                                if rm>=2:
+                                        rm=0
+                                time.sleep(.5)
+                except IOError:
+                        print "Button Error"
+
+
+
+		if t==0:
+			print "Συλλογή δεδομένων, παρακαλώ περιμένετε..."
+			getSensorData("Temperature")
+			new_text="Getting data..."
+			setRGB(50, 50, 50)
+			t=1
+
+		else:
+			if new_t != t:
+				new_t=t
+				timevalue = datetime.datetime.fromtimestamp((timestamp/1000.0)-3600*(t-1))
+				strtime=timevalue.strftime('%Y-%m-%d %H:%M:%S')
+				print strtime
+ 		
+			val=sensorValues[rm][new_t-1]
+			showTemperature(val,pin1[rm],pin2[rm])
+			new_text= strtime + ": " +  str(float("{0:.2f}".format(val))) 
+			setRGB(R[rm], G[rm], B[rm])
+
+
+		if text != new_text:
+			text = new_text
+			print "φωτεινότητα:",properties.the_rooms[rm], val
+
+			#print "LCD show:", text
+			setText(text)
+
+
+
+
+try:
+    	main()
+except KeyboardInterrupt:
+    	exitapp = True
+    	raise
