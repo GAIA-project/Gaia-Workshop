2e8908909beb3a306338e00143526499bf011e3b
LPFraile <lidiapf0@gmail.com>
2018-03-13 15:06:30 +0200

    Review of Workshop 1

--- code/GAIA-Workshop1.2.button.py
+++ code/GAIA-Workshop1.2.button.py
@@ -17,7 +17,7 @@ pinBWhite = 5
 
 exitapp = False
 
-Interruptor = 0
+Button = 8
 
 #Declare the LED pins as OUTPUT
 grovepi.pinMode(pinAYellow, "OUTPUT")
@@ -26,16 +26,14 @@ grovepi.pinMode(pinBYellow, "OUTPUT")
 grovepi.pinMode(pinBWhite, "OUTPUT")
 
 #Declare the Switch pins as INPUT
-grovepi.pinMode(Interruptor, "INPUT")
+grovepi.pinMode(Button, "INPUT")
 
 # initialize the screen
-
 setText("Starting..")
 setRGB(50, 50, 50)
 time.sleep(1)
 
-mode = 0
-
+open = 0
 text = ""
 new_text = ""
 
@@ -51,14 +49,11 @@ def closeAllLeds():
 closeAllLeds()
 
 def loop():
-    global pinAYellow,pinAWhite,pinBYello,pinBWithe,new_text
-    value = grovepi.analogRead(Interruptor)
-    #interruptor off
-    if value < 800:
-	new_text="All leds close"
-	closeAllLeds()
-    #interruptor on	
-    else:
+    global pinAYellow,pinAWhite,pinBYello,pinBWithe,new_text,open
+    if grovepi.digitalRead(Button):
+	open =1
+    #when click the button
+    if open == 1:
 	new_text="Leds  Open"
 	#open led A color blue
 	grovepi.digitalWrite(pinAYellow, 1)
@@ -68,6 +63,11 @@ def loop():
 	grovepi.digitalWrite(pinBYellow, 1)
 	grovepi.digitalWrite(pinBWhite, 0)
 
+	open=0
+    	
+    else:
+	new_text="All leds close"
+	closeAllLeds()
 
 def main():
     global text,new_text
---
+++ code/GAIA-Workshop1.2.old.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+sys.path.append(os.getcwd())
+import time
+import gaia_text
+
+import grovepi
+from grove_rgb_lcd import *
+
+# from grove_rgb_lcd import setText_norefresh as setText
+
+pin1 = [2, 4]
+pin2 = [3, 5]
+
+exitapp = False
+
+Interruptor = 0
+
+# Motor=6
+poten = 0
+for i in [0, 1]:
+    grovepi.pinMode(pin1[i], "OUTPUT")
+    grovepi.pinMode(pin2[i], "OUTPUT")
+
+grovepi.pinMode(Interruptor, "INPUT")
+
+# initialize the screen
+
+setText("Starting..")
+setRGB(50, 50, 50)
+time.sleep(1)
+
+mode = 0
+
+text = ""
+new_text = ""
+
+def closeAllLeds():
+    global pin1, pin2
+    for i in [0, 1]:
+        grovepi.digitalWrite(pin1[i], 0)
+        grovepi.digitalWrite(pin2[i], 0)
+
+closeAllLeds()
+
+def loop():
+    global mode, led, new_text
+    value = grovepi.analogRead(Interruptor)
+     	   
+    #interruptor off
+    if value < 800:
+		mode=0
+    #interruptor on 
+    else:
+        mode=1
+
+    if mode==0:
+		new_text="All leds close"
+		closeAllLeds()
+    if mode==1:
+		new_text="Leds  Open"
+		#open led 1 color blue
+		grovepi.digitalWrite(pin1[0], 1)
+		grovepi.digitalWrite(pin2[0], 0)
+		#open led 2 color blue
+		grovepi.digitalWrite(pin1[1], 1)
+		grovepi.digitalWrite(pin2[1], 0)
+
+def main():
+    global text,new_text
+    while not exitapp:
+        loop()
+        if text != new_text:
+            text = new_text
+            print "settext:", text
+            setText(text)
+
+try:
+    main()
+except KeyboardInterrupt:
+    exitapp = True
+    raise
