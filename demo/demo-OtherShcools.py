aa8731c95ae4d8b66ce638cfd03bbed9bf9d9484
LPFraile <lidiapf0@gmail.com>
2018-04-30 13:14:56 +0300

    Small cleanups

--- demo/demo-OtherShcools.py
+++ demo/demo-OtherShcools.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
 import sys
@@ -38,14 +39,14 @@ for i in range(len(properties.school)):
 def updateData(resource):
     summary = sparkworks.summary(resource)
     value = summary["minutes60"][1]
-    return(float("{0:.1f}".format(float(value))))
+    return float("{0:.1f}".format(float(value)))
 
 
 def updateSiteData(site, param):
     resource = sparkworks.siteResource(site, param)
     summary = sparkworks.summary(resource)
     value = summary["minutes5"][0]
-    return(float("{0:.1f}".format(float(value))))
+    return float("{0:.1f}".format(float(value)))
 
 
 def getData(idx):
@@ -69,17 +70,17 @@ def getData(idx):
     for i in range(len(rooms)):
         motion[i] = updateSiteData(rooms[i], "Motion")
 
-    return(power, temp, humid, motion[0], motion[1], motion[2], True)
+    return [power, temp, humid, motion[0], motion[1], motion[2], True]
 
-    
+
 def getDataThread():
-    global school_data
+    global exitapp, school_data
     while not exitapp:
         for idx in range(len(properties.school)):
             school_data[idx] = getData(idx)
             time.sleep(10)
 
-                
+
 for idx in range(len(properties.school)):
     school_data[idx] = getData(idx)
 
@@ -106,7 +107,7 @@ def showMotion(motion, a, b):
 
 
 def main():
-    global school_idx, school_data
+    global exitapp, school_idx, school_data
     power_max = 10000000
     temp_max = 40
     humid_max = 100
@@ -115,27 +116,22 @@ def main():
 
         if idx_changed:
             setText(properties.school[school_idx]['name'])
-            #print("School Index:" + str(school_idx))
-            #print(properties.school[school_idx]['name'])
-            #for i in range(len(school_data[school_idx])):
-                #print(school_data[school_idx][i])
-                
+
         if idx_changed or school_data[school_idx][6]:
+            print "School Index:" + str(school_idx)
+            print properties.school[school_idx]['name']
+            for i in range(len(school_data[school_idx])):
+                print school_data[school_idx][i]
             power_led = mapValueToLeds(power_max, school_data[school_idx][0], 8)
             temp_led  = mapValueToLeds(temp_max,  school_data[school_idx][1], 8)
             humid_led = mapValueToLeds(humid_max, school_data[school_idx][2], 8)
             arduinoGauge.write(power_led, temp_led, humid_led)
             for i in range(3):
                 showMotion(school_data[school_idx][i+3], pin1[i], pin2[i])
-            school_data[school_idx] = [ school_data[school_idx][0],
-                                        school_data[school_idx][1],
-                                        school_data[school_idx][2],
-                                        school_data[school_idx][3],
-                                        school_data[school_idx][4],
-                                        school_data[school_idx][5], False ]
+            school_data[school_idx][6] = False
 
         try:
-            if(grovepi.digitalRead(button)):
+            if grovepi.digitalRead(button):
                 idx_changed = True
                 school_idx = school_idx + 1
                 if school_idx >= len(properties.school):
