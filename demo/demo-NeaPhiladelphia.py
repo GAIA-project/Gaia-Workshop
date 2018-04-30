df31f6d718b5656d9105cdb6314cff4ce97c7f77
LPFraile <lidiapf0@gmail.com>
2018-04-30 13:10:14 +0300

    Disable printing to console

--- demo/demo-NeaPhiladelphia.py
+++ demo/demo-NeaPhiladelphia.py
@@ -37,9 +37,12 @@ R = [255, 255, 0]
 G = [0, 128, 255]
 B = [255, 0, 0]
 
-setText(gaia_text.loading_data)
-setRGB(60, 60, 60)
-
+# initialize the screen
+setText("starting..")
+setRGB(50, 50, 50)
+time.sleep(.1)
+text = ""
+new_text=""
 
 for i in [0, 1, 2]:
     grovepi.pinMode(pin1[i], "OUTPUT")
@@ -49,24 +52,16 @@ for i in [0, 1, 2]:
 
 def updateData(resource):
     summary = sparkworks.summary(resource)
-    #latestTime = time.gmtime(summary["latestTime"] / 1000)
-    val = (summary["minutes60"][0])
+    val = (summary["minutes60"][1])
     return (float("{0:.1f}".format(float(val))))
 
 #Take new values from the data base 
 def updateSiteData(site, param):
     
- #   print site, param
     resource = sparkworks.siteResource(site, param)
-#    print "resource:",resource	
     summary = sparkworks.summary(resource)
     val = (summary["minutes5"][0])
     return (float("{0:.1f}".format(float(val))))
-#def updateSiteData(site, param):
-#    resource = sparkworks.siteResourceDevice(site, param)
-#    latest = sparkworks.latest(resource)
-#    latest_value = float("{0:.1f}".format(float(latest["latest"])))
-#    return latest_value
 
 
 
@@ -80,9 +75,9 @@ def getData():
     data = updateData(total_humid)
     humidity = data
     for i in [0, 1, 2]:
-#	    print rooms[i]['name']	
+	    #print rooms[i]['name']	
             motion[i] = updateSiteData(rooms[i], "Motion")
-    print power, temperature, humidity
+    #print power, temperature, humidity
 
 def threaded_function(arg):
     global  power
@@ -90,11 +85,11 @@ def threaded_function(arg):
         getData()
 
 
-print ("Username: \n\t%s\n" % properties.username).encode("utf8", "replace")
-print "Sensors:"
+#print ("Username: \n\t%s\n" % properties.username).encode("utf8", "replace")
+#print "Sensors:"
 
 arduinoGauge.connect()
-arduinoGauge.write(0, 0, 0)
+arduinoGauge.write(0, 0,0)
 
 sparkworks.connect(properties.username, properties.password)
 main_site = sparkworks.main_site()
@@ -102,24 +97,22 @@ total_power = sparkworks.total_power(main_site)
 total_temp = sparkworks.total_site(main_site, "Temperature")
 total_humid = sparkworks.total_site(main_site, "Relative Humidity")
 rooms = sparkworks.select_rooms(properties.the_rooms)
-#for room in rooms:
-#	print room["name"]
+
+#print "\t%s" % total_power["uri"]
+#print ""
+#print "\t%s" % total_temp["uri"]
+#print ""
+#print "\t%s" % total_humid["uri"]
+#print ""
+#print "όνομα χρήστη:\n\t%s\n" % properties.username
+#print properties.username
+#print "Επιλεγμένη αίθουσα:"
+#for room in properties.the_rooms:
+#    print '\t%s' % room.decode('utf-8')
+#print '\n'
 
 
-print "\t%s" % total_power["uri"]
-print ""
-print "\t%s" % total_temp["uri"]
-print ""
-print "\t%s" % total_humid["uri"]
-print ""
-print "όνομα χρήστη:\n\t%s\n" % properties.username
-print "Επιλεγμένη αίθουσα:"
-for room in properties.the_rooms:
-    print '\t%s' % room.decode('utf-8')
-print '\n'
-
-
-print "Collecting data, please wait..."
+#print "Collecting data, please wait..."
 getData()
 
 thread = Thread(target=threaded_function, args=(10,))
@@ -141,10 +134,10 @@ def showMotion(motion,a,b):
         	grovepi.digitalWrite(b, 0)
 
 def main():
-    global temperature,humidity,power,motion	
+    global temperature,humidity,power,motion,text,new_text	
     time.sleep(1)
     while not exitapp:
-	maxPower = 5000000
+	maxPower = 10000000
 	maxTemp = 40
 	maxHumid = 100
         ledPower=map_value_to_leds(maxPower, power, 8)
@@ -152,18 +145,23 @@ def main():
         ledHumid=map_value_to_leds(maxHumid, humidity, 8)
         arduinoGauge.write(ledPower,ledTemp,ledHumid)
         time.sleep(0.5)
-        print "Total Power:    " + str(power) + " Wh"
-        print "Total Temperature avg:    " + str(temperature) + " oC"
-        print "Total Humidity avg:    " + str(humidity) + " %RH"
-	print "Motion purple:	"+str(motion[0])
-	print "Motion orange:	"+str(motion[1])
-	print "Motion green:	"+str(motion[2])
+ #       print "Total Power:    " + str(power) + " Wh"
+	new_text="1Gym. Nea Phyl. Power:"+str(float("{0:.2f}".format(float(power/1000000))))+"kWh"
+ #       print "Total Temperature avg:    " + str(temperature) + " oC"
+ #       print "Total Humidity avg:    " + str(humidity) + " %RH"
+#	print "Motion purple:	"+str(motion[0])
+#	print "Motion orange:	"+str(motion[1])
+#	print "Motion green:	"+str(motion[2])
 
 	for i in [0, 1, 2]:
         	showMotion(motion[i], pin1[i], pin2[i]);
+	if text != new_text:
+            text = new_text
+ #           print "settext", text
+            setText(text)
 
 	#Motion
-        time.sleep(5)
+        time.sleep(1)
 try:
     main()
 except KeyboardInterrupt:
